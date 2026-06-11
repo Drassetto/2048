f13050835e4c654522fce484519260d6e99f2f00
manugm09 <manuelgarciamantello09@gmail.com>
2026-06-10 23:29:13 -0300

    Update header.h

--- header.h
+++ header.h
@@ -31,17 +31,15 @@ void liberar_stack(UndoStack* stack);
 #define FLECHA_IZQUIERDA 75
 #define FLECHA_DERECHA 77
 InstanciaTablero deslizar_izquierda(InstanciaTablero instancia);
-InstanciaTablero compactar_izquierda(InstanciaTablero instancia);
+InstanciaTablero compactar_horiz(InstanciaTablero instancia);
 InstanciaTablero mover_izquierda(InstanciaTablero instancia);
 
 InstanciaTablero deslizar_derecha(InstanciaTablero instancia);
-InstanciaTablero compactar_derecha(InstanciaTablero instancia);
 InstanciaTablero mover_derecha(InstanciaTablero instancia);
 
 InstanciaTablero deslizar_arriba(InstanciaTablero instancia);
-InstanciaTablero compactar_arriba(InstanciaTablero instancia);
+InstanciaTablero compactar_vertical(InstanciaTablero instancia);
 InstanciaTablero mover_arriba(InstanciaTablero instancia);
 
 InstanciaTablero deslizar_abajo(InstanciaTablero instancia);
-InstanciaTablero compactar_abajo(InstanciaTablero instancia);
 InstanciaTablero mover_abajo(InstanciaTablero instancia);
